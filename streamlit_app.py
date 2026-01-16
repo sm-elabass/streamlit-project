51cf508c8e68f3a17bd8b8334436864c3901dc63
Sidi Mohamed EL ABASS <sidim6946@gmail.com>
2026-01-16 10:49:18 +0100

    Test Bar chat in streamlit app py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.write('HelloMBADIA !')
+import streamlit as st
+import pandas as pd
+ 
+st.header("Bar Chart")
+ 
+data = {"a":[23, 12, 78, 4, 54], "b":[0, 13 ,88, 1, 3], 
+"c":[45, 2, 546, 67, 56]}
+ 
+df = pd.DataFrame(data)
+df
+st.bar_chart(data=df)
